b1896b78da495007c4c29207c3740e04c9eb029a
Diego H <dhdezr@gmail.com>
2026-07-22 23:23:04 +0200

    Invalidate broken controller cache

--- sw.js
+++ sw.js
@@ -1,11 +1,38 @@
-const CACHE = 'canto-de-barro-v3';
-const CORE = ['./', './index.html', './styles.css', './src/world.js', './src/game.js', './manifest.webmanifest'];
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting())));
-self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
+const CACHE = 'canto-de-barro-v4-physics';
+const CORE = [
+  './',
+  './index.html',
+  './styles.css',
+  './src/world.js',
+  './src/game.js',
+  './src/player-physics.js',
+  './src/player-controller.js',
+  './manifest.webmanifest',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-    if (response.ok && new URL(event.request.url).origin === location.origin) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
-    return response;
-  })));
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html'))),
+  );
 });
