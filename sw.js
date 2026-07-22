3c3aa3b34f5c4f35f523a507ac2dc2f4d4994def
Diego H <dhdezr@gmail.com>
2026-07-22 22:50:43 +0200

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'canto-de-barro-v3';
+const CORE = ['./', './index.html', './styles.css', './src/world.js', './src/game.js', './manifest.webmanifest'];
+self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting())));
+self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok && new URL(event.request.url).origin === location.origin) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+    return response;
+  })));
+});
